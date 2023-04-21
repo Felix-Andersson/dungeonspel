5959ca251e645f98969dd5c6ab6f5425731589f7
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-04-21 11:29:25 +0200

    v 2.2.2

--- variables.rb
+++ variables.rb
@@ -9,65 +9,43 @@ $enemies = ["Goblin", "Skeleton", "Microsoft Bengt", "Shaman"]
 $bosses = ["Troll", "Skeleton King", "Orc lord"]
 $ultimateBoss = "Mattias"
 
-#enemy stats
-$goblin_health = 40
-$goblin_weapon = 1
-$goblin_armor = 3
-$goblin_worth = 30
+#enemy stats    [health, weapon, armor, worth]
+$goblin = [40, 1, 3, 30]
 
-$skeleton_health = 40
-$skeleton_weapon = 3
-$skeleton_armor = 0
-$skeleton_worth = 30
+$skeleton = [40, 3, 0, 30]
 
-$microsoftBengt_health = 60
-$microsoftBengt_weapon = 5
-$microsoftBengt_armor = 1
-$microsoftBengt_worth = 30
+$microsoftBengt = [60, 5, 1, 30]
 
-$shaman_health = 30
-$shaman_weapon = 6
-$shaman_armor = 0
-$shaman_worth = 30
+$shaman = [30, 6, 0, 30]
 
-#boss stats
-$troll_health = 300
-$troll_weapon = 9
-$troll_armor = 13
-$troll_worth = 100
+#boss stats     [health, weapon, armor, worth]
+$troll = [300, 9, 13, 100]
 
-$skeletonKing_health = 200
-$skeletonKing_weapon = 15
-$skeletonKing_armor = 3
-$skeletonKing_worth = 100
+$skeleton = [200, 15, 3, 100]
 
-$orcLord_health = 250
-$orcLord_weapon = 12
-$orcLord_armor = 9
-$orcLord_worth = 100
+$orcLord = [250, 12, 9, 100]
 
-# Mattias stats
-$ultimateBoss_health = 1000
-$ultimateBoss_weapon = 25
-$ultimateBoss_armor = 30
-$ultimateBossWorth = 1000000
+$mattias = [1000, 25, 30, 1000000]
 
 # Shop items
-$electron_canon = 100000
-$laser_pointer = 500
-$violation = 4
-$stick = 6
-$spear = 15
-$great_axe = 25
+#weapons    [damage, cost]
+$electron_canon = [100000]
+$laser_pointer = [500]
+$violation = [4]
+$stick = [6]
+$spear = [15]
+$great_axe = [25]
 
 $weapons = [$electron_canon, $laser_pointer, $violation, $stick, $spear, $great_axe]
 
+#armor
 $nti_shirt = 10
 $boots = 5
 $kevlar = 20
 
 $armor = [$nti_shirt, $boots, $kevlar]
 
+#food
 $panerad_fisk = 10
 $meatball = 5
 $jarpar = 6
