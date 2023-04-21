3d62213ad15f3090d3c096be01811b19a09589e7
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-04-21 22:47:03 +0200

    v 3.0

--- main.rb
+++ main.rb
@@ -7,68 +7,137 @@ def isQuit(input)
     return true
 end
 
-# input loop
-def shop!(user_input, health, armor, weapon)
-    user_input = ""
-    isQuit(user_input)
-    while user_input != "leave"
-        puts "Nasir: What do you need traveler?"
+def isShop(user_input)
+    if user_input.downcase == "yes"
+        return true
+    end
+    return false
+end
+
+def fight(enemy)
+    enemy_health = enemy[1]
+    while enemy_health > 0
+        user_input = ""
+        writeLine "\nYou are facing a #{enemy[0]}, what will you do?"
         puts "--- --- --- --- --- --- --- ---"
-        puts "  Weapons[1]  Armor[2]  Food[3]"
+        puts "Attack[1]  Eat[2]  Quit Game[3]"
+        puts "--- --- --- --- --- --- --- ---"
+        user_input = gets.chomp
+
+        case user_input
+        when "1"
+            writeLine("You attacked, dealing #{$weapon} damage!")
+            enemy_health -= $weapon                                             #Lägg till så att $weapon får damagen från items i inventory också
+            writeLine("The #{enemy[0]}'s health is at #{enemy_health}")
+        when "2"
+            writeLine("Homelander: Yummers.")
+        when "quit", "exit", "3"
+            puts ("Aight' have a good one, later!")
+            exit(true)
+        else
+            raise "This wasn't supposed to be put in here!!"
+        end
+    end
+    puts "He ded."
+    $money += enemy[4]
+    puts "Your money is #{$money}g"
+end
+
+def shop()
+    user_input = ""
+    while isQuit(user_input)
+        puts "\nNasir: What do you need traveler?"
+        puts "--- --- --- --- --- --- --- ---"
+        puts " Weapons[1]  Armor[2]  Food[3] "
         puts "--- --- --- --- --- --- --- ---"
 
         user_input = gets.chomp
-        if user_input.downcase == "quit" || user_input.downcase == "exit" #|| input == 4
-            break
-        elsif user_input == "1"
+        case user_input
+        when "1"     #switch-statement (annat sätt att skriva if-satser på typ)
             showWeapons()
+        when "2"
+            showArmors()
+        when "3"
+            showFood()
+        when "quit", "exit"
+            break
+        else
+            raise "This wasn't supposed to be put in here!!"
         end
+
+        #Visa inventory
+        showInventory()
     end
 end
 
 def showWeapons()
     i=0
-    puts "Nasir: What do you need traveler?"
+    puts "Nasir: What do you need traveler? [current money: #{$money}]"
     puts "--- --- --- --- --- --- --- ---"
     while i<$weapons.length
-        print $weapons[i][0]
-        puts " [#{i+1}]"
+        puts "+#{$weapons[i][1]} #{$weapons[i][0]} #{$weapons[i][2]}g, [#{i+1}]"
         i += 1
     end
     puts "--- --- --- --- --- --- --- ---"
 
     user_input = gets.chomp
-    isQuit(user_input)
-    if user_input == "1"
-        $money -= $weapons[0][2]
-        $inventory << $weapons[0]
-        p $inventory
+    if user_input.downcase == "quit" || user_input.downcase == "exit"
+        puts ("Aight' have a good one, later!")
+        exit(true)                                          #stänger hela programmet
     end
-
+    $inventory[0] << $weapons[user_input.to_i-1]         #Kan lägga till raise om användaren skriven in string
+    $money -= $weapons[user_input.to_i-1][2]
 end
 
-def isShop(user_input)
-    if user_input.downcase != "yes"
-        return false
+def showArmors()
+    i=0
+    puts "Nasir: What do you need traveler? [current money: #{$money}]"
+    puts "--- --- --- --- --- --- --- ---"
+    while i<$armors.length
+        puts "+#{$armors[i][1]} #{$armors[i][0]} #{$armors[i][2]}g, [#{i+1}]"
+        i += 1
     end
-    return true
-end
+    puts "--- --- --- --- --- --- --- ---"
 
-def fight!(enemy, health, weapon, armor)
-    user_input = ""
-    isQuit(user_input)
-    puts "You are facing a #{enemy[0]}, what will you do?"
-    puts "--- --- --- --- --- --- --- ---"
-    puts "  Attack[1]  Eat[2]  Leave[3]"
-    puts "--- --- --- --- --- --- --- ---"
     user_input = gets.chomp
-    if user_input == "1" || user_input.downcase == "attack"
-        puts "He ded."
-        $money += enemy[4]
+    if user_input.downcase == "quit" || user_input.downcase == "exit"
+        puts ("Aight' have a good one, later!")
+        exit(true)                                          #stänger hela programmet
     end
+    $inventory[1] << $armors[user_input.to_i-1]         #Kan lägga till raise om användaren skriven in string
+    $money -= $armors[user_input.to_i-1][2]
+end
 
-    if user_input == "2" || user_input.downcase == "eat"
-        writeLine("Homelander: Yummers.")
+def showFood()
+    i=0
+    puts "Nasir: What do you need traveler? [current money: #{$money}g]"
+    puts "--- --- --- --- --- --- --- ---"
+    while i<$food.length
+        puts "+#{$food[i][1]} #{$food[i][0]} #{$food[i][2]}g, [#{i+1}]"
+        i += 1
+    end
+    puts "--- --- --- --- --- --- --- ---"
+
+    user_input = gets.chomp
+    if user_input.downcase == "quit" || user_input.downcase == "exit"
+        puts ("Aight' have a good one, later!")
+        exit(true)                                          #stänger hela programmet
+    end
+    $inventory[2] << $food[user_input.to_i-1]         #Kan lägga till raise om användaren skriven in string
+    $money -= $food[user_input.to_i-1][2]
+end
+
+def showInventory()
+    print "Inventory:"
+    i=0
+    while i < $inventory.length
+        puts "\n-"
+        j=0
+        while j < $inventory[i].length
+            print "#{j+1}. #{$inventory[i][j][0]}  "
+            j+=1
+        end
+        i+=1
     end
 end
 
@@ -78,34 +147,32 @@ def writeLine(string)
 end
     
 def main()
-    
     user_input = ""
     writeLine("Ah we have finally found a teknikare, what is thou name?")
     name = gets.chomp
     while isQuit(user_input)
         writeLine("Saittam: I've heard that there is an impostor among us, stay alert!")
         writeLine("*Jakob comes running*")
-        writeLine("Jakob: Big trouble up ahead, I spotted some goblins vent!")
+        writeLine("Jakob: Big trouble up ahead, I spotted some monsters venting!")
         writeLine("Saittam: We must stop them, ahem, I mean you must stop them #{name}!\n")
+
+        # Fight
         enemy = $enemies[rand($enemies.length)]
         writeLine("A #{enemy[0]} notices you!")
-        writeLine("*The #{enemy[0]} shrieks!*")
-        
-        fight!(enemy, $health, $weapon, $armor)
-        puts "Your money is #{$money}"
-        user_input = gets.chomp
-        isQuit(user_input)
-    end
-    
-    user_input = ""
-    writeLine("Do you want to shop?")
-    user_input = gets.chomp
-    isQuit(user_input)
-    while isShop(user_input)
-        shop!(user_input, $health, $weapon, $armor)
-    end
+        writeLine("*The #{enemy[0]} grunts.*")
+        fight(enemy)
 
+        # Shop
+        writeLine("Do you want to shop? [yes/no]")
+        user_input = gets.chomp
+        if isShop(user_input)
+            shop()
+        end
+
+        writeLine("Finished shopping ey? Or maybe you didn't need anything. ['quit' to exit game]")
+        user_input = gets.chomp
+    end
 end
 
 
-main()
+main()  #Kör programmet
--- variables.rb
+++ variables.rb
@@ -1,57 +1,48 @@
-#player
+# Player
 $health = 100
 $armor = 0
-$weapon = 3
+$weapon = 10
 $money = 0
+#inventory  [[weapons],[armor],[food]]
+$inventory = [[],[],[]]
 
-#enemies
-$bosses = ["Troll", "Skeleton King", "Orc lord"]
-$ultimateBoss = "Mattias"
+# Enemy stats    [name, health, weapon, armor, worth]
+$goblin = ["Goblin", 10, 1, 3, 30]
+$skeleton = ["Skeleton", 10, 3, 0, 30]
+$microsoft_bengt = ["Microsoft Bengt", 20, 5, 1, 30]
+$shaman = ["Shaman", 15, 6, 0, 30]
 
-#enemy stats    [name, health, weapon, armor, worth]
-$goblin = ["Goblin", 40, 1, 3, 30]
+$enemies = [$goblin, $skeleton, $microsoft_bengt, $shaman]
 
-$skeleton = ["Skeleton", 40, 3, 0, 30]
+# Boss stats     [name, health, weapon, armor, worth]
+$troll = ["Troll", 300, 9, 13, 100]
+$skeleton_king = ["Skeleton King", 200, 15, 3, 100]
+$orc_lord = ["Orc Lord", 250, 12, 9, 100]
+$mattias = ["Mattias", 1000, 25, 30, 1000000]
 
-$microsoftBengt = ["Microsoft Bengt", 60, 5, 1, 30]
-
-$shaman = ["Shaman", 30, 6, 0, 30]
-
-$enemies = [$goblin, $skeleton, $microsoftBengt, $shaman]
-
-#boss stats     [health, weapon, armor, worth]
-$troll = [300, 9, 13, 100]
-
-$skeleton = [200, 15, 3, 100]
-
-$orcLord = [250, 12, 9, 100]
-
-$mattias = [1000, 25, 30, 1000000]
+$bosses = [$troll, $skeleton_king, $orc_lord, $mattias]
 
 # Shop items
 #weapons    [name, damage, cost]
-$electron_cannon = ["Electron cannon", 100000, 100]
-$laser_pointer = ["Laser pointer", 500, 50]
+$electron_cannon = ["Electron Cannon", 100000, 100]
+$laser_pointer = ["Laser Pointer", 500, 50]
 $violation = ["Violation", 4, 30]
 $stick = ["Stick", 6, 10]
 $spear = ["Spear", 15, 40]
-$great_axe = ["Great axe", 25, 70]
+$great_axe = ["Great Axe", 25, 70]
 
 $weapons = [$electron_cannon, $laser_pointer, $violation, $stick, $spear, $great_axe]
 
-#armor
-$nti_shirt = 10
-$boots = 5
-$kevlar = 20
+#armor  [name, resistance, cost]
+$nti_shirt = ["NTI Shirt", 12, 400]
+$lader_dojor = ["Läder Dojor", 5, 20]
+$kevlar = ["Kevlar", 20, 50]
 
-$armor = [$nti_shirt, $boots, $kevlar]
+$armors = [$nti_shirt, $lader_dojor, $kevlar]
 
-#food
-$panerad_fisk = 10
-$meatball = 5
-$jarpar = 6
+#food   [name, health, cost]
+$panerad_fisk = ["Panerad Fisk", 10, 20]
+$meatball = ["Meatball", 15, 30]
+$jarpar = ["Jarpar", 5, 10]
 
 $food = [$panerad_fisk, $meatball, $jarpar]
-
-#inventory
-$inventory = []
