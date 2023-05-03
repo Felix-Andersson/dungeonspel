8977b4bde376a851ac4c0d0b8e2cb89656a0f9ee
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-05-03 10:35:23 +0200

    v 4.0
    näst sista lektionen

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative 'variables'
+require_relative 'color_text'
 
 def isQuit(input)
     if input.downcase == "quit" || input.downcase == "exit" #|| input == 4
@@ -20,21 +21,22 @@ def fight(enemy)
     while enemy_health > 0 && $health > 0
         displayPlayerStatus()
         user_input = ""
-        writeLine "\nYou are facing a #{enemy[0]}, what will you do?"
+        writeLine("\nYou are facing a #{enemy[0]}, what will you do?".magenta)
         puts "--- --- --- --- --- --- --- ---"
-        puts "Attack[1]  Eat[2]  Quit Game[3]"
+        print "Attack[1] ".bg_red.bold
+        print " Eat[2] ".bg_green.bold
+        puts "Quit Game[3]".bg_blue.bold
         puts "--- --- --- --- --- --- --- ---"
         user_input = gets.chomp
 
         case user_input
         when "1"
             if $inventory[0][0] == "Violation"
-                writeLine("You: #{$violations[rand(0..6)]}!")
+                writeLine("You: #{$violations[rand(0..6)]}!".blink.yellow)
             end
             enemy_health -= $inventory[0][1] + enemy[3] + rand(-5..5)                                         #Lägg till så att $weapon får damagen från items i inventory också
-            writeLine("You attacked, dealing #{$inventory[0][1]} damage!")
+            writeLine("You attacked, dealing #{$inventory[0][1]} damage!".green)
         when "2"
-            writeLine("Homelander: Yummers.")
             eat()
         when "quit", "exit", "3"
             puts ("Aight' have a good one, later! Ses imorgon! Ha de gött! hej!")
@@ -46,7 +48,7 @@ def fight(enemy)
         #Enemy attack
         enemy_attack = enemy[2] - $inventory[1][1] + rand(-1..5)
         $health -= enemy_attack
-        writeLine("#{enemy[0]} attacked, dealing #{enemy_attack} damage!")
+        writeLine("#{enemy[0]} attacked, dealing #{enemy_attack} damage!".bg_red)
         print "#{enemy[0]}'s Health: #{enemy_health}"
     end
     
@@ -54,22 +56,79 @@ def fight(enemy)
         puts "\nHe ded."
         $enemies_killed += 1
         $money += enemy[4]
-        puts "Your money is #{$money}g" 
+        puts "Your money is #{$money}g".italic
+        if enemy[0] == "Troll" || enemy[0] == "Skeleton King" || enemy[0] == "Orc Lord"
+            bosses_killed += 1
+        end
     else
-        puts "\nWe ded."
+        puts "\nYou died.".red
         $money -= (0.2*$money)
-        puts "Your money is #{$money}g" 
+        puts "Your money is #{$money}g".italic 
         #kanske förlorar ett random item från inventory eller något
+        $dead = true
     end
 
-    writeLine("Enemies killed: #{$enemies_killed}")
+    writeLine("Enemies killed: #{$enemies_killed}".italic)
+end
+
+def fightMattias(mattias)
+    actions = 0
+    mattias_health = mattias[1]
+    while mattias_health > 0 && $health > 0
+        displayPlayerStatus()
+        user_input = ""
+        writeLine("\nOh no, You are facing #{mattias[0]}, what will you do?".magenta)
+        puts "--- --- --- --- --- --- --- ---"
+        puts "Attack[1]  Eat[2]  Quit Game[3]".bold
+        puts "--- --- --- --- --- --- --- ---"
+        user_input = gets.chomp
+
+        case user_input
+        when "1"
+            actions += 1
+            if $inventory[0][0] == "Violation"
+                writeLine("You: #{$violations[rand(0..6)]}!".blink)
+            end
+            mattias_health -= $inventory[0][1] + mattias[3] + rand(-5..5)                                         #Lägg till så att $weapon får damagen från items i inventory också
+            writeLine("You attacked, dealing #{$inventory[0][1]} damage!".green)
+        when "2"
+            actions += 1
+            eat()
+        when "quit", "exit", "3"
+            puts ("Aight' have a good one, later! Ses imorgon! Ha de gött! hej!")
+            exit(true)
+        else
+            raise "This wasn't supposed to be put in here!!"
+        end
+
+        #Enemy attack
+        mattias_attack = mattias[2] - $inventory[1][1] + rand(-1..5)
+        $health -= mattias_attack
+        writeLine("#{mattias[0]} attacked, dealing #{mattias_attack} damage!".red)
+        print "#{mattias[0]}'s Health: #{mattias_health}"
+    end
+    
+    if enemy_health < 1
+        writeLine("\nMattias is defeated. You are victorious."-green.bold)
+        $enemies_killed += 1
+        $money += mattias[4]
+        puts "Your money is #{$money}g".italic
+        $bosses_killed += 1
+    else
+        writeLine("\nWe ded.".red.bold)
+        $money -= (0.2*$money)
+        puts "Your money is #{$money}g".italic 
+        #kanske förlorar ett random item från inventory eller något
+        mattias_deaths += 1
+        $dead = true
+    end
 end
 
 def eat()
-    writeLine("Which food would you like to consume?")
+    writeLine("Which food would you like to consume?".bold)
     i=0
-    while i < $inventory[2].length
-        print "#{i+1}. #{$inventory[2][i][0]}  "        #vad, vilket, namnet
+    while i < $food_inventory.length
+        print "#{i+1}. #{$food_inventory[i][0]}  "        #vad, vilket, namnet
         if (i % 3) == 0 && i != 0                               #sänker ner det en rad var 
             puts ""
         end
@@ -77,11 +136,15 @@ def eat()
     end
     
     user_input = gets.chomp.to_i
-    if user_input <= 0 && user_input > $inventory[2].length
+    if user_input <= 0 && user_input > $food_inventory.length
         raise "This wasn't supposed to be put in here!!"
     end
     
-    $health += $inventory[2][user_input][1]         #food, vilken, heatlht restored
+    $health += $food_inventory[user_input-1][1]     #food, vilken, heatlht restored
+    if $health > 100
+        $health = 100
+    end
+
 end
 
 def shop()
@@ -89,7 +152,7 @@ def shop()
     while isQuit(user_input)
         puts "\nNasir: What do you need traveler?"
         puts "--- --- --- --- --- --- --- ---"
-        puts " Weapons[1]  Armor[2]  Food[3] "
+        puts " Weapons[1]  Armor[2]  Food[3] ".bold
         puts "--- --- --- --- --- --- --- ---"
 
         user_input = gets.chomp
@@ -133,7 +196,7 @@ def showWeapons()
         raise "This wasn't supposed to be put in here!!"
     end
     if $money >= $weapons[user_input.to_i-1][2]
-        writeLine("Bought #{$weapons[user_input.to_i-1][0]}")
+        writeLine("Bought #{$weapons[user_input.to_i-1][0]}".italic)
         $inventory[0] = $weapons[user_input.to_i-1]         #Kan lägga till raise om användaren skriven in string
         $money -= $weapons[user_input.to_i-1][2]    
     else
@@ -164,7 +227,7 @@ def showArmors()
         raise "This wasn't supposed to be put in here!!"
     end
     if $money >= $armors[user_input.to_i-1][2] 
-        writeLine("Bought #{$armors[user_input.to_i-1][0]}")
+        writeLine("Bought #{$armors[user_input.to_i-1][0]}".italic)
         $inventory[1] = $armors[user_input.to_i-1]         #Kan lägga till raise om användaren skriven in string
         $money -= $armors[user_input.to_i-1][2]    
     else
@@ -194,65 +257,116 @@ def showFood()
         raise "This wasn't supposed to be put in here!!"
     end
     if $money >= $food[user_input.to_i-1][2] 
-        writeLine("Bought #{$food[user_input.to_i-1][0]}")
-        $inventory << $food[user_input.to_i-1]
+        writeLine("Bought #{$food[user_input.to_i-1][0]}".italic)
+        $food_inventory << $food[user_input.to_i-1]
         $money -= $food[user_input.to_i-1][2]    
     else
         writeLine("Nasir: Get out you miserable pleb and stop wasting my time!")
     end
 end
 
-def writeLine(string)
-    temp = string+"\n"
-    temp.each_char {|c| putc c ; sleep 0.01}
-end
-
 def displayPlayerStatus()
     writeLine("\n")
-    writeLine("Health: #{$health}")
-    writeLine("Weapon: #{$inventory[0][0]}, Dmg: #{$inventory[0][1]}")
-    writeLine("Armor: #{$inventory[1][0]}, Defence: #{$inventory[1][1]}")
+    writeLine("Health: #{$health}".italic)
+    writeLine("Weapon: #{$inventory[0][0]}, Dmg: #{$inventory[0][1]}".italic)
+    writeLine("Armor: #{$inventory[1][0]}, Defence: #{$inventory[1][1]}".italic)
     writeLine("Food: ")
     i=0
-    while i < $inventory[2].length
-        print "#{i+1}. #{$inventory[2][i][0]}  "        #vad, vilket, namnet
+    while i < $food_inventory.length
+        print "#{i+1}. #{$food_inventory[i][0]}  ".italic        #vad, vilket, namnet
         if (i % 3) == 0 && i != 0                               #sänker ner det en rad var 
             puts ""
         end
         i+=1
     end
 end
-    
+
 def main()
     user_input = ""
-    writeLine("Ah we have finally found a teknikare, what is thou name?")
-    name = gets.chomp
+    writeLine("Ah we have finally found a teknikare, what is thou name?".bold)
+    name = gets.chomp.italic.blink
     while isQuit(user_input)
-        writeLine("Saittam: I've heard that there is an impostor among us, stay alert!")
-        writeLine("*Jakob comes running*")
+        if $enemies_killed == 0
+        writeLine("Saittam: I've heard that there is an impostor among us, stay alert!".cyan)
+        writeLine("*Jakob comes running*".bold)
         writeLine("Jakob: Big trouble up ahead, I spotted some monsters venting! Ses imorgon! Ha de gött! hej!")
-        writeLine("Saittam: We must stop them, ahem, I mean you must stop them #{name}!\n")
-
+        writeLine("Saittam: We must stop them, ahem, I mean you must stop them #{name}!\n".cyan)
+        end
         # Fight
         # Check if bosses
-        if $enemies_killed == 10
+        if $dead
+            $health = 50
+            $dead = false
+            writeLine("Welcome back #{name}! Are you okay?")
+        end
+        if $bosses_killed == 3
+            if mattias_deaths == 0
+                writeLine("*Jakob enters Saittam's room*".bold)
+                writeLine("Saittam: Greetings Jakob, did you bring a pen and paper?".cyan)
+                writeLine("Jakob: I actually brought something better!")
+                writeLine("*Jakob takes out his computer*".bold)
+                writeLine("Saittam: Y-you brought a computer?".cyan)
+                writeLine("Jakob: Yes, it is much more efficient!")
+                writeLine("Saittam is standing in front of the whiteboard with a pen, sharpened to perfection.".cyan)
+                writeLine("Saittam: Aäeeh, skärmar i botten!".cyan)
+                writeLine("Jakob looks att Saittam with a distraught expression")
+                writeLine("Saittam: Inga digitala hjälpmedel! Penna o papper det gäller samtliga!".cyan)
+                writeLine("*Saittam throws the sharpened pen into the skull of a frightened Jakob*".bold)
+                writeLine("Jakob is no more".bold)
+
+                
+
+                writeLine("*Jimmy comes running to you*")
+                writeLine("Jimmy: #{name}! #{name}! Jakob is dead!")
+                writeline("#{name}: What??")
+                writeline("Jimmy: I found him in Saittams room!")
+                
+                writeLine("38 minutes later...")
+                
+                writeLine("*You enter Saittam's room*")
+                writeLine("Saittam: #{name} I take it Jakob has been destroyed. I must say you're sooner than expected.")
+                writeLine("#{name}: Saittam! Explain your actions. What did you do to Jakob? Are you really on our side?")
+                writeLine("*Saittam gives you a furious glance and you realize your mistake".bold)
+                writeLine("Saittam: Skärmar. Skärmar, I BOTTEN!".cyan)
+                writeLine("*Saittam reveals himself to actually be Mattias \n
+                    and he throws his pen with full force at your skärm which is destroyed".bold)
+                writeLine("Mattias: PAPPER OCH PENNA! Inga digitala hjälpmedel".blue)
+                writeLine("You got the papper och penna weapon")
+            elsif mattias_deaths > 0
+                writeLine("*You return from the grim reapers embrace and try once more to defeat this great menace.".bold)
+                writeLine("Mattias: Hahahahahaha, hehehehehe, ja tänkte att någon skulle skratta kanske".blue)
+            end
+            $inventory[0] = $papper_penna
+            enemy = $bosses[3]
+            fight(enemy)
+        end
+
+        if $bosses_killed == 4
+            writeLine("You won and secured the skärmar of the world!")
+            isQuit("quit")
+        end
+        
+        if $enemies_killed == 5 && $bosses_killed < 3
             enemy = $bosses[rand($bosses.length)]
             writeLine("A towering #{enemy[0]} notices you!")
             writeLine("*The #{enemy[0]} roars with fury and the ground shakes.*")
-        elsif $enemies_killed == 20
+        elsif $enemies_killed == 15 && $bosses_killed < 3
             enemy = $bosses[rand($bosses.length)]
             writeLine("A towering #{enemy[0]} notices you!")
             writeLine("*The #{enemy[0]} roars with fury and the ground shakes.*")
-        elsif $enemies_killed == 30
+        elsif $enemies_killed == 20 && $bosses_killed < 3
             enemy = $bosses[rand($bosses.length)]
             writeLine("A towering #{enemy[0]} notices you!")
             writeLine("*The #{enemy[0]} roars with fury and the ground shakes.*")
-        else
+        elsif $bosses_killed < 3
             enemy = $enemies[rand($enemies.length)]
             writeLine("A #{enemy[0]} notices you!")
             writeLine("*The #{enemy[0]} grunts.*")
         end
-        fight(enemy)
+        if $bosses_killed < 3
+            fight(enemy)
+        end
+        
         
         # Shop
         writeLine("Do you want to shop? [yes/no]")
@@ -260,7 +374,7 @@ def main()
         if isShop(user_input)
             shop()
         end
-
+        
         writeLine("Finished shopping ey? Or maybe you didn't need anything. ['quit' to exit game]")
         user_input = gets.chomp
     end
--- variables.rb
+++ variables.rb
@@ -1,15 +1,3 @@
-# Player
-$health = 100
-$max_health = 100
-$default_weapon = ["Rusty sword", 10, 0]
-$default_armor = ["Half naked outfit", 1, 0]
-$money = 0
-$enemies_killed = 0
-$bosses_killed = 0
-#inventory  [weapons,armor,[food]]
-$inventory = [$default_weapon,$default_armor,[$jarpar]]
-$food_inventory = [$jarpar]
-
 # Enemy stats    [name, health, weapon, armor, worth]
 $goblin = ["Goblin", 30, 3, 3, 30]
 $skeleton = ["Skeleton", 25, 6, 2, 30]
@@ -19,21 +7,21 @@ $shaman = ["Shaman", 15, 10, 0, 30]
 $enemies = [$goblin, $skeleton, $microsoft_bengt, $shaman]
 
 # Boss stats     [name, health, weapon, armor, worth]
-$troll = ["Troll", 800, 30, 13, 200]
-$skeleton_king = ["Skeleton King", 700, 35, 3, 200]
-$orc_lord = ["Orc Lord", 750, 40, 9, 200]
-$mattias = ["Mattias", 5000, 25, 30, 1000000]
+$troll = ["Troll", 400, 18, 13, 200]
+$skeleton_king = ["Skeleton King", 300, 20, 3, 200]
+$orc_lord = ["Orc Lord", 525, 40, 9, 200]
+$mattias = ["Mattias", 1000, 25, 30, 1000000]
 
 $bosses = [$troll, $skeleton_king, $orc_lord, $mattias]
 
 # Shop items
 #weapons    [name, damage, cost]
 $electron_cannon = ["Electron Cannon", 200, 700]
-$laser_pointer = ["Laser Pointer", 85, 300]
+$laser_pointer = ["Laser Pointer", 70, 250]
 $violation = ["Violation", 15, 70]
 $stick = ["Stick", 12, 30]
 $spear = ["Spear", 25, 100]
-$great_axe = ["Great Axe", 50, 200]
+$great_axe = ["Great Axe", 50, 180]
 #mattias weapons, story weapons
 $skarm = ["Skärm", 10000, 0]
 $papper_penna = ["Papper och Penna", 100, 0]
@@ -43,9 +31,9 @@ $violations = ["Dumma dig", "Baen", "Bro finna regret", "No", "Yes", "Euugghh", 
 $weapons = [$electron_cannon, $laser_pointer, $violation, $stick, $spear, $great_axe]
 
 #armor  [name, resistance, cost]
-$nti_shirt = ["NTI Shirt", 12, 150]
-$lader_dojor = ["Läder Dojor", 5, 85]
-$kevlar = ["Kevlar", 20, 200]
+$nti_shirt = ["NTI Shirt", 12, 80]
+$lader_dojor = ["Läder Dojor", 8, 40]
+$kevlar = ["Kevlar", 20, 150]
 $almost_naked_outfit = ["Almost naked outfit", 5, 30]
 
 $armors = [$nti_shirt, $lader_dojor, $kevlar, $almost_naked_outfit]
@@ -54,7 +42,21 @@ $armors = [$nti_shirt, $lader_dojor, $kevlar, $almost_naked_outfit]
 $panerad_fisk = ["Panerad Fisk", 20, 30]
 $meatball = ["Meatball", 15, 25]
 $jarpar = ["Jarpar", 10, 15]
-$knackebrod_frukost = ["Frukost knäckebröd", 50, 100]
+$knackebrod_frukost = ["Frukost knäckebröd", 50, 60]
 $feferoni = ["Feferoni", 5, 10]
 
 $food = [$panerad_fisk, $meatball, $jarpar, $knackebrod_frukost, $feferoni]
+
+# Player
+$health = 100
+$max_health = 100
+$default_weapon = ["Rusty sword", 10, 0]
+$default_armor = ["Half naked outfit", 1, 0]
+$money = 0
+$enemies_killed = 0
+$bosses_killed = 0
+$mattias_deaths = 0
+#inventory  [weapons,armor,[food]]
+$inventory = [$default_weapon, $default_armor]
+$food_inventory = [$jarpar]
+$dead = false
