22d56f3b7eded86cc792cdc686e0c585bff71806
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-05-14 18:55:27 +0200

    10.1

--- window.rb
+++ window.rb
@@ -26,11 +26,28 @@ set background: "#7bb59c"
     y: (Window.height-100),
     color: "yellow"
 )
-name = ""
+
+@enemy = Rectangle.new(
+    x: (Window.width-50-150),
+    y: (Window.height-160-250),
+    width: 150,
+    height: 250,
+    color: "green"
+)
+$name = ""
 nameGotten = false
+dialog = 0
 
-def 
-
+def textDisplay(number)
+    dialogArray = [
+        "Saittam: I've heard that there is an impostor among us, stay alert!",
+        "*Jakob comes running*",
+        "Jakob: Big trouble up ahead, I spotted some monsters venting!",
+        "Jakob: Ses imorgon! Ha de gött! hej!",
+        "Saittam: We must stop them, ahem, I mean you must stop them #{$name}!\n"
+    ]
+    @text.text = dialogArray[number]
+end
 
 # A key was pressed
 on :key_down do |event|
@@ -38,23 +55,25 @@ on :key_down do |event|
         if event.key == "return"
             nameGotten = true
             @input.text = ""
-            @text.text = "Alright then #{name}, let us begin your adventure!"
+            @text.text = "Alright then #{$name}, let us begin your adventure!"
         elsif event.key == "backspace"
-            name = name[0...-1]
-            @input.text = name
+            $name = $name[0...-1]
+            @input.text = $name
         elsif "abcdefghijklmnopqrstuvwxyzåäö1234567890".include? event.key
-            name += event.key
-            name[0] = name[0].upcase
-            @input.text = name
+            $name += event.key
+            $name[0] = $name[0].upcase
+            @input.text = $name
         end
     end
 end
 
+
+
 on :mouse_down do |event|
     if (event.button == :left) && (@textBox.contains? @mouse_x_pos, @mouse_y_pos)
         #pressed inside textbox
-        
-        @text.text = "Saittam: I've heard that there is an impostor among us, stay alert!"
+        textDisplay(dialog)
+        dialog += 1
     end
 
 end
